ca88b34660d0c9ac3c99ae3bbb6b6f1da171f054
Dylan Murillo Rios <deymoloper@gmail.com>
2023-10-24 20:17:19 -0500

    añadiendo los archivos necesarios, para el get all products

---
+++ Backend/CodeCina.API/CodeCina.API/Controllers/Products/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using CodeCina.Application.Queries.Products;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCina.API.Controllers.Products
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("queryGetAllProducts")]
+        public async Task<IActionResult> GetAllProducts ()
+        {
+            try
+            {
+                var result = await _mediator.Send(new getAllProductsQuery());
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error: ", ex);
+            }
+        }
+    }
+}
---
+++ Backend/CodeCina.API/CodeCina.Application/Queries/Products/getAllProductsQuery.cs
@@ -0,0 +1,50 @@
+﻿using CodeCina.Application.Dtos.Products;
+using CodeCina.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCina.Application.Queries.Products
+{
+    public class getAllProductsQuery: IRequest<List<productDto>>
+    {
+
+    }
+    public class getAllProductsQueryHandler: IRequestHandler<getAllProductsQuery, List<productDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly ILogger<getAllProductsQueryHandler> _logger;
+
+        public getAllProductsQueryHandler(IApplicationDbContext context, ILogger<getAllProductsQueryHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<List<productDto>> Handle(getAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("getAllProductsQueryHandler Started");
+            var query = await _context.Productos
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            var products = query.Select(x => new productDto
+            {
+                Nombre = x.Nombre,
+                Cantidad = x.Cantidad,
+                IdMedida = x.IdMedida,
+                IdProducto = x.IdProducto,
+                IdTipoProducto = x.IdTipoProducto,
+            }
+            ).ToList();
+
+            _logger.LogDebug("getAllProductsQueryHandler Finished");
+            return products;
+        }
+    }
+}
